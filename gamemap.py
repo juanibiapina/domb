c758c50077db6fcfcceac6984ca09cfaade0dd67
juanibiapina <juanibiapina@gmail.com>
2012-05-24 00:41:08 -0300

    Add some style. To the code, not to the game. Yep.

--- gamemap.py
+++ gamemap.py
@@ -3,6 +3,7 @@ import random
 GROUND_TILE = (9, 31)
 HOLE_TILE = (13, 32)
 
+
 class GameMap(object):
     def __init__(self, data, obstacles, tile_set):
         self._data = data
@@ -16,23 +17,25 @@ class GameMap(object):
             self._tile_set.blit_tile(screen, tile, pos)
 
     def walkable(self, x, y):
-        return (x, y) in self._data.keys() and (x,y) not in self._obstacles.keys()
+        return (x, y) in self._data.keys() and (x, y) not in self._obstacles.keys()
+
 
 def generate_map(tile_set):
     return GameMap(generate_rectangle(15, 10, GROUND_TILE), generate_obstacles(15, 10), tile_set)
 
+
 def generate_rectangle(width, height, tile):
     rect = {}
     for i in xrange(width):
-      for j in xrange(height):
-        rect[(i,j)] = tile
+        for j in xrange(height):
+            rect[(i, j)] = tile
     return rect
+
 
 def generate_obstacles(width, height):
     obstacles = {}
     for i in xrange(width):
         for j in xrange(height):
-            if(random.randint(0,10) < 2):
-                obstacles[(i,j)] = HOLE_TILE
-
-    return obstacles 
+            if(random.randint(0, 10) < 2):
+                obstacles[(i, j)] = HOLE_TILE
+    return obstacles
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from pygame.locals import *
 import pygame
-import time
 import sys
 import random
 
@@ -9,48 +8,52 @@ from gamemap import *
 COW_TILE = (5, 4)
 HUNTER_TILE = (18, 16)
 
+
 class TileSet(object):
     TILE_SIZE = 32
-    
+
     def __init__(self):
         self.tiles_image = pygame.image.load('tileset.png')
 
     def blit_tile(self, target_surface, tile_index, dest):
-        x, y = map(lambda coord: coord*self.TILE_SIZE, tile_index)
-        transformed_dest = map(lambda coord: coord*self.TILE_SIZE, dest)
-        target_surface.blit(self.tiles_image, transformed_dest, 
+        x, y = map(lambda coord: coord * self.TILE_SIZE, tile_index)
+        transformed_dest = map(lambda coord: coord * self.TILE_SIZE, dest)
+        target_surface.blit(self.tiles_image, transformed_dest,
                             Rect(x, y, self.TILE_SIZE, self.TILE_SIZE))
 
+
 class Cow(object):
-    def __init__(self, tile_set, the_map):
+    def __init__(self, tile_set, game_map):
         self.tile_set = tile_set
-        self.the_map = the_map
+        self.game_map = game_map
         self.pos = (0, 0)
 
     def draw(self, surface):
         self.tile_set.blit_tile(surface, COW_TILE, self.pos)
-    
+
     def move(self, dx, dy):
         new_pos_x = self.pos[0] + dx
         new_pos_y = self.pos[1] + dy
-        
-        if (self.the_map.walkable(new_pos_x, new_pos_y)):
+
+        if (self.game_map.walkable(new_pos_x, new_pos_y)):
             self.pos = new_pos_x, new_pos_y
 
+
 class Hunter(object):
-    def __init__(self, tileset, the_map):
+    def __init__(self, tileset, game_map):
         self.tileset = tileset
-        self.the_map = the_map
+        self.game_map = game_map
         self.pos = (7, 7)
 
     def draw(self, surface):
         self.tileset.blit_tile(surface, HUNTER_TILE, self.pos)
-    
+
     def move(self, dx, dy):
         new_pos_x = self.pos[0] + dx
         new_pos_y = self.pos[1] + dy
-        if (self.the_map.walkable(new_pos_x, new_pos_y)): 
+        if (self.game_map.walkable(new_pos_x, new_pos_y)):
             self.pos = new_pos_x, new_pos_y
+
 
 def handle_input(cow, hunter):
     ev = pygame.event.poll()
@@ -67,22 +70,23 @@ def handle_input(cow, hunter):
             sys.exit(0)
         hunter.move(random.randint(-1, 1), random.randint(-1, 1))
 
+
 def main():
     screen = pygame.display.set_mode((640, 480))
     pygame.display.init()
 
     tile_set = TileSet()
-    the_map = generate_map(tile_set)
+    game_map = generate_map(tile_set)
 
-    cow = Cow(tile_set, the_map)
-    hunter = Hunter(tile_set, the_map)
-    
+    cow = Cow(tile_set, game_map)
+    hunter = Hunter(tile_set, game_map)
+
     running = True
     while running:
         handle_input(cow, hunter)
 
-        screen.fill((0,0,0))
-        the_map.draw(screen)
+        screen.fill((0, 0, 0))
+        game_map.draw(screen)
         cow.draw(screen)
         hunter.draw(screen)
 
