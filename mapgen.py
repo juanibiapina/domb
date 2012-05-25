a8b0474c159c8f7fb56e3e851ee47309bfbff887
Kao Félix <kcfelix@gmail.com>
2012-05-25 19:09:05 -0300

    Add range class for more meaningful ranges.

--- mapgen.py
+++ mapgen.py
@@ -7,14 +7,18 @@ from pygame import draw
 
 AREA = Rect(0, 0, 500, 500)
 
+class Range(object):
+    def __init__(self, min, max):
+        self.min = min
+        self.max = max
+
 class Room(object):
-    MIN_ROOM_SIZE = 2
-    MAX_ROOM_SIZE = 50
+    ROOM_SIZE = Range(2, 50)
 
     def __init__(self, pos):
         self.boundary = Rect(0, 0, 
-                             randint(self.MIN_ROOM_SIZE, self.MAX_ROOM_SIZE), 
-                             randint(self.MIN_ROOM_SIZE, self.MAX_ROOM_SIZE))
+                             randint(self.ROOM_SIZE.min, self.ROOM_SIZE.max), 
+                             randint(self.ROOM_SIZE.min, self.ROOM_SIZE.max))
         self.boundary.center = pos
 
     def draw(self, surface):
