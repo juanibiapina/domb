fb6cd21e2fc2e5a4256cf1ed49d89b310b5458da
Kao Félix <kcfelix@gmail.com>
2012-05-25 19:20:19 -0300

    Create new room at east of existing one.

--- mapgen.py
+++ mapgen.py
@@ -12,23 +12,43 @@ class Range(object):
         self.min = min
         self.max = max
 
+    def random(self):
+        return randint(self.min, self.max)
+    
 class Room(object):
     ROOM_SIZE = Range(2, 50)
+    ROOM_DISTANCE = Range(10, 50)
+
 
     def __init__(self, pos):
-        self.boundary = Rect(0, 0, 
-                             randint(self.ROOM_SIZE.min, self.ROOM_SIZE.max), 
-                             randint(self.ROOM_SIZE.min, self.ROOM_SIZE.max))
+        self.boundary = Rect(0, 0, self.ROOM_SIZE.random(), self.ROOM_SIZE.random())
         self.boundary.center = pos
 
     def draw(self, surface):
         draw.rect(surface, Color("green"), self.boundary)
 
+    def create_new_room_at_east(self):
+        new_room = Room(self.boundary.center)
+        new_room.boundary.left = self.boundary.right + self.ROOM_DISTANCE.random()
+        return new_room
+
+def generate_rooms():
+    room_list = []
+    room = Room(AREA.center)
+    new_room = room.create_new_room_at_east()
+
+    room_list.append(room)
+    room_list.append(new_room)
+
+    return room_list
+
 def preview():
     import pygame
     screen = pygame.display.set_mode(AREA.size)
 
-    Room(AREA.center).draw(screen)
+    for room in generate_rooms():
+        room.draw(screen)
+
     pygame.display.flip()
 
     while True:
