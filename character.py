9823251b480a21a43c003b65e0c43d840ddef150
juanibiapina <juanibiapina@gmail.com>
2012-05-27 03:38:26 -0300

    Moved stuff around. I really don't know where to put it.

--- character.py
+++ character.py
@@ -1,10 +1,13 @@
+from d20 import roll
+
+
 class Character(object):
     def __init__(self, tile_index, dungeon):
         self.tile_index = tile_index
         self.dungeon = dungeon
         self.pos = dungeon.get_random_position()
         self.ai = None
-        self.hp = 2
+        self.hp = 2  # cat hp
 
     def draw(self, surface, tile_set):
         tile_set.blit_tile(surface, self.tile_index, self.pos)
@@ -21,3 +24,17 @@ class Character(object):
     def update(self):
         if self.ai:
             self.ai.update(self)
+
+    def calculate_damage(self):
+        return roll(3)  # unarmed strike
+
+    def calculate_attack(self):
+        return roll(20)  # base attack, no modifiers
+
+    def calculate_ac(self):
+        return 10  # base ac, no modifiers
+
+    def damage(self, damage):
+        self.hp -= damage
+        if self.hp <= 0:
+            print "incapacitated"
--- d20.py
+++ d20.py
@@ -1,22 +1,18 @@
 from random import randint
 
-BASE_AC = 10
-
 
 def roll(sides):
     return randint(1, sides)
 
 
 def resolve_damage(origin, target):
-    damage = roll(3)  # unarmed strike
-    target.hp -= damage
-    if target.hp <= 0:
-        print "incapacitated"
+    damage = origin.calculate_damage()
+    target.damage(damage)
 
 
 def resolve_attack(origin, target):
-    attack = roll(20)
-    ac = BASE_AC
+    attack = origin.calculate_attack()
+    ac = target.calculate_ac()
     if attack >= ac:
         resolve_damage(origin, target)
     else:
