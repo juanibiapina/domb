16dec2eb92ff4797ac6d039909b4fa3455f2c68c
Bruno Maioli <bmaioli@gmail.com>
2012-05-29 22:59:07 -0300

    Critical hit FTW

--- domb/d20.py
+++ domb/d20.py
@@ -9,8 +9,8 @@ def roll(sides):
     return randint(1, sides)
 
 
-def resolve_damage(origin, target):
-    damage = origin.calculate_damage()
+def resolve_damage(origin, target, critical):
+    damage = origin.calculate_damage() * critical
     target.damage(damage)
     return damage
 
@@ -18,6 +18,12 @@ def resolve_damage(origin, target):
 def resolve_attack(origin, target):
     attack = origin.calculate_attack()
     ac = target.calculate_ac()
+    critical = 1
+
+    if attack == 20 and origin.calculate_attack() >= ac:  # critical hit!
+        logger.info('Critical HIT!')
+        critical = 2
+
     if attack >= ac:  # hit!
         damage = resolve_damage(origin, target)
         logger.info('%s attacked %s - attack: %d / ac: %d / damage: %d', origin.get_name(), target.get_name(), attack, ac, damage)
