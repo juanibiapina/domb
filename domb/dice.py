7f472789988387dcb557c12e01bff28fd931b7dc
juanibiapina <juanibiapina@gmail.com>
2012-05-29 22:42:18 -0300

    Fixing emacs plus non standard operating system crap.

--- domb/dice.py
+++ domb/dice.py
@@ -1,14 +1,15 @@
 from random import randint
 import re
 
+
 class Dice(object):
     DICE_REGEXP = re.compile("([0-9]+|1/2)d([0-9]+)")
-    
+
     def __init__(self, description):
         base_dice, modifiers = self._split_description(description)
         self._parse_dice(base_dice)
         self._calculate_modifiers(modifiers)
-        
+
     def roll(self):
         dice_roll = sum(randint(1, self.sides) for i in range(self.num_dices))
         return int(dice_roll * self.multiplier) + self.modifier
@@ -27,7 +28,7 @@ class Dice(object):
         else:
             self.num_dices = int(num_dices)
             self.multiplier = 1
-            
+
         self.sides = int(m.group(2))
 
     def _calculate_modifiers(self, modifiers):
