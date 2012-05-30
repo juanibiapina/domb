f3d35d9326fd75f8684c1f502c5540a96fb7dc72
Bruno Maioli <bmaioli@gmail.com>
2012-05-29 23:35:23 -0300

    Improve critical hit message

--- domb/d20.py
+++ domb/d20.py
@@ -9,7 +9,7 @@ def roll(sides):
     return randint(1, sides)
 
 
-def resolve_damage(origin, target, critical):
+def resolve_damage(origin, target, critical=1):
     damage = origin.calculate_damage() * critical
     target.damage(damage)
     return damage
@@ -18,14 +18,15 @@ def resolve_damage(origin, target, critical):
 def resolve_attack(origin, target):
     attack = origin.calculate_attack()
     ac = target.calculate_ac()
-    critical = 1
-
-    if attack == 20 and origin.calculate_attack() >= ac:  # critical hit!
-        logger.info('Critical HIT!')
-        critical = 2
 
     if attack >= ac:  # hit!
-        damage = resolve_damage(origin, target)
-        logger.info('%s attacked %s - attack: %d / ac: %d / damage: %d', origin.get_name(), target.get_name(), attack, ac, damage)
+        if attack == 20:
+            critical_confirmation = origin.calculate_attack()
+            critical = 2 if (critical_confirmation >= ac) else 1
+            damage = resolve_damage(origin, target, critical)
+            logger.info('%s attacked %s - attack: %d (critical: %d) / ac: %d / damage (x%d): %d', origin.get_name(), target.get_name(), attack, critical_confirmation, ac, critical, damage)
+        else:
+            damage = resolve_damage(origin, target)
+            logger.info('%s attacked %s - attack roll: %d / ac: %d / damage roll: %d', origin.get_name(), target.get_name(), attack, ac, damage)
     else:  # no hit
         logger.info('%s attacked %s - attack: %d / ac: %d', origin.get_name(), target.get_name(), attack, ac)
