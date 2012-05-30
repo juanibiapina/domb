e349d6183013da8ab518bb475a18ce0de4b95708
Bruno Maioli <bmaioli@gmail.com>
2012-05-29 22:33:11 -0300

    Logging attack and damage

--- domb/d20.py
+++ domb/d20.py
@@ -1,4 +1,8 @@
 from random import randint
+import logging
+
+
+logger = logging.getLogger('console')
 
 
 def roll(sides):
@@ -8,10 +12,12 @@ def roll(sides):
 def resolve_damage(origin, target):
     damage = origin.calculate_damage()
     target.damage(damage)
+    return damage
 
 
 def resolve_attack(origin, target):
     attack = origin.calculate_attack()
     ac = target.calculate_ac()
     if attack >= ac:
-        resolve_damage(origin, target)
+        damage = resolve_damage(origin, target)
+    logger.info('%s attacked %s - attack roll: %d / damage roll: %d', origin.get_name(), target.get_name(), attack, damage)
