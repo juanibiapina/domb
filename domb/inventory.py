9caf184807aff46ea51dcf5b4841400cf1b502de
Bruno Maioli <bmaioli@gmail.com>
2012-06-06 18:31:55 -0300

    Stacking potions to kill diablo.

--- domb/inventory.py
+++ domb/inventory.py
@@ -9,7 +9,11 @@ class Inventory(object):
         self.current_slot = 0
 
     def add_item(self, item):
-        self.items.append(item)
+        inv_item = filter(lambda x: x[0].get_name() == item.get_name(), self.items)
+        if inv_item:
+            self.items[self.items.index(inv_item[0])] = (item, inv_item[0][1] + 1)
+        else:
+            self.items.append((item, 1))
 
     def next_slot(self):
         self.current_slot = (self.current_slot + 1) % self.capacity
@@ -22,13 +26,10 @@ class Inventory(object):
 
     def slots_and_items(self):
         slots = self.capacity
-        return izip_longest(range(slots), self)
+        return izip_longest(range(slots), self.items)
 
     def __len__(self):
         return len(self.items)
 
     def __getitem__(self, key):
         return self.items[key]
-
-    def __iter__(self):
-        return iter(self.items)
--- domb/view/inventory.py
+++ domb/view/inventory.py
@@ -1,6 +1,7 @@
 from pygame import draw
 from pygame.locals import Color
 from vec2d import Vec2d
+from pygame.font import SysFont
 
 
 class InventoryView(object):
@@ -8,6 +9,7 @@ class InventoryView(object):
     SELECTED_ITEM_COLOR = Color("white")
     SLOT_BORDER_COLOR = Color("#4D2C12")
     SLOT_BACKGROUND_COLOR = Color("#AE5E21")
+    SLOT_FONT_COLOR = Color("white")
 
     def __init__(self, inventory):
         self.pos = Vec2d(1, 1)
@@ -23,7 +25,7 @@ class InventoryView(object):
         for slot, item in self.inventory.slots_and_items():
             self._draw_slot(screen, slot)
             if item:
-                item.draw(screen, self.pos + Vec2d(slot, 0))
+                self._draw_item(screen, slot, item)
 
     def next_item(self):
         self.inventory.next_slot()
@@ -55,5 +57,20 @@ class InventoryView(object):
         draw.rect(screen, self.SLOT_BACKGROUND_COLOR, self._slot_rect(slot))
 
     def _slot_rect(self, slot):
-        pos = (self.pos * self.SLOT_SIZE) + Vec2d(slot * self.SLOT_SIZE, 0)
+        pos = self._slot_pos(slot)
         return (pos.x, pos.y, self.SLOT_SIZE, self.SLOT_SIZE)
+
+    def _slot_pos(self, slot):
+        return (self.pos * self.SLOT_SIZE) + Vec2d(slot * self.SLOT_SIZE, 0)
+
+    def _draw_item(self, screen, slot, item):
+        item[0].draw(screen, self.pos + Vec2d(slot, 0))
+        self._draw_item_quantity(screen, slot, item[1])
+
+    def _draw_item_quantity(self, screen, slot, quantity):
+        quantity_surface = SysFont('Arial', 12, bold=True).render(str(quantity), False, self.SLOT_FONT_COLOR)
+        screen.blit(quantity_surface, (self._item_quantity_pos(quantity_surface.get_size(), slot)))
+
+    def _item_quantity_pos(self, surface_size, slot):
+        pos = self._slot_pos(slot)
+        return (pos.x + self.SLOT_SIZE - surface_size[0] - 1, pos.y + self.SLOT_SIZE - surface_size[1] - 1)
