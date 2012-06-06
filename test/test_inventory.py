e92475476351a917f98a3f952795dd08bab2da98
Bruno Maioli <bmaioli@gmail.com>
2012-06-06 18:53:37 -0300

    Fixing le damn tests.

--- test/test_inventory.py
+++ test/test_inventory.py
@@ -1,19 +1,31 @@
 from domb.inventory import Inventory
+from mock import Mock
 
-inv = Inventory(capacity=3)
+item_a = Mock(name="potion")
+item_b = Mock(name="armor")
 
 
 def test_add_items():
-    inv.add_item("item 1")
-    inv.add_item("item 2")
+    inv = Inventory(capacity=3)
+    inv.add_item(item_a)
+    inv.add_item(item_b)
     assert len(inv) == 2
 
 
+def test_stack_items():
+    inv = Inventory(capacity=3)
+    inv.add_item(item_a)
+    inv.add_item(item_a)
+    assert inv[0][1] == 2
+
+
 def test_indexable():
-    inv.add_item("item 1")
-    assert inv[0] == "item 1"
+    inv = Inventory(capacity=3)
+    inv.add_item(item_a)
+    assert inv[0][0] == item_a
 
 
 def test_selection():
+    inv = Inventory(capacity=3)
     inv.next_slot()
     assert inv.is_selected(1)
