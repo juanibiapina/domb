774796e2d042c54d0b77fdda9e3a5e2a967628fb
juanibiapina <juanibiapina@gmail.com>
2012-06-07 18:28:26 -0300

    Moving tests around.

--- test/character/test_xp.py
+++ test/character/test_xp.py
@@ -1,21 +1,19 @@
 from domb.character.xp import XP
-from mock import Mock
 
-class TestXP:
 
-  def test_xp_level_up_using_decimal(self):
+def test_xp_level_up_using_decimal():
     xp = XP(1)
-    xp.increase(1.0/4)
+    xp.increase(1.0 / 4)
     assert xp.xp == 75
-  
-  def test_xp_level_up(self):
+
+
+def test_xp_level_up():
     xp = XP(1)
     xp.increase(1)
     assert xp.xp == 300
 
-  def test_xp_level_up_even_more(self):
+
+def test_xp_level_up_even_more():
     xp = XP(5)
     xp.increase(2)
     assert xp.xp == 500
-
-
