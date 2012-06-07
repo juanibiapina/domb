5e4db81caba9763c0c04cdadc4bc35859f6e79a3
Bruno Tavares <bruno.exz@gmail.com>
2012-06-07 10:26:14 -0300

    With great power comes great experiences

--- test/test_xp.py
+++ test/test_xp.py
@@ -6,10 +6,16 @@ class TestXP:
   def test_xp_level_up_using_decimal(self):
     xp = XP(1)
     xp.increase(1.0/4)
+    assert xp.xp == 75
+  
+  def test_xp_level_up(self):
+    xp = XP(1)
+    xp.increase(1)
     assert xp.xp == 300
-    
-    xp.increase(2)
-    assert xp.xp == 1200
 
+  def test_xp_level_up_even_more(self):
+    xp = XP(5)
+    xp.increase(2)
+    assert xp.xp == 500
 
 
